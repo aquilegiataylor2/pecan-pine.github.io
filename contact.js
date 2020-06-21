a98dcba70e88a1c1274c266f882be16f368e1ae6
Nathan Taylor <pecan.pine@gmail.com>
2020-06-21 00:56:06 -0700

    changed picture to smaller file size, updated contact with links

--- contact.js
+++ contact.js
@@ -3,7 +3,8 @@ function contact(output){
     <div> Contact Information:</div>
     <div>Nathan Taylor</div>
     <div>pecan.pine@gmail.com</div>
-    <div>https://github.com/pecan-pine</div>
-    <div>https://pecan-pine.github.io</div>
+    <div><a href="https://github.com/pecan-pine">https://github.com/pecan-pine</a></div>
+    <div><a href="https://pecan-pine.github.io">https://pecan-pine.github.io</a></div>
+    <div><a href="https://www.linkedin.com/in/nathan-taylor-74312a184">LinkedIn </a></div>
     `;
 }
