532860e8d9f6e47b5dc3d2d37dea7bf6204bf8e8
Nathan Taylor <pecan.pine@gmail.com>
2020-06-22 13:48:35 -0700

    beginnings of new page

--- shared/contact.js
+++ shared/contact.js
@@ -3,7 +3,7 @@ function contact(output){
     <div> Contact Information:</div>
     <div>Nathan Taylor</div>
     <div>pecan.pine@gmail.com</div>
-    <div><a href="https://github.com/pecan-pine" target="_blank" rel="noopener noreferrer">Github</a></div>
+    <div><a href="https://github.com/pecan-pine" target="_blank" rel="noopener noreferrer">GitHub</a></div>
     <!-- <div><a href="https://pecan-pine.github.io">https://pecan-pine.github.io</a></div> -->
     <div><a href="static/resume.pdf" target="_blank" rel="noopener noreferrer"> Resume </a> </div> 
     <div><a href="https://www.linkedin.com/in/nathan-taylor-74312a184"  target="_blank" rel="noopener noreferrer">LinkedIn </a></div>
